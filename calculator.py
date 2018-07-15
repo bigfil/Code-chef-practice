88a6fef86f1a434fdb242ab055d0f4ba6ed8f13c
bigfil <filipniezgoda99@gmail.com>
2018-07-15 17:40:25 +0100

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,14 @@
+#https://www.codechef.com/problems/URCALC
+
+A = int(input())
+B = int(input())
+C = str(input())
+
+if C == "+":
+    print(A+B)
+elif C == "-":
+    print(A-B)
+elif C == "*":
+    print(A*B)
+elif C == "/":
+    print(A/B)
