dde8490a9871eb483735c611391b9b38052be4bf
bigfil <filipniezgoda99@gmail.com>
2018-08-19 21:40:59 +0100

    Create ormatrix.cpp

---
+++ ormatrix.cpp
@@ -0,0 +1,22 @@
+/*
+https://www.codechef.com/COOK96B/problems/ORMATRIX
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int T,N,M;
+    vector<vector<int>> matrix;
+    cin >> T;
+    
+    for(int i=0; i<T; i++){
+        matrix.clear();
+        cin >> N;
+        cin >> M;
+        matrix.resize(N, std::vector<int>(M, 0));
+        
+    }
+}
